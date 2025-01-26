c5e681f6f6c717791b21ad676e807b337cdb44c1
msaad1682 <msaadiftikhar67@gmail.com>
2025-01-26 19:09:22 +0500

    Experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+import project1 from "../assets/projects/ems.png";
+import project2 from "../assets/projects/electradefense.png";
+import project3 from "../assets/projects/react.png";
 import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
@@ -9,68 +9,67 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "Nov 2024 - Present",
+    role: "React  Developer",
+    company: "Innovative Saudia.",
+    description: `Led a team in developing and maintaining web applications using JavaScript and React.js. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["React.js", "Tailwind CSS"],
   },
   {
-    year: "2022 - 2023",
+    year: "Oct 2023 - Nov 2024",
+    role: "PHP Developer",
+    company: "Green Professional ",
+    description: `Designed and developed user interfaces for web applications using Bootstrap. Designed and implemented RESTful APIs for data communication. Worked closely with backend to integrate frontend components with PHP mySql. Implemented responsive designs and optimized frontend and backendend perfomance .`,
+    technologies: ["HTML", "CSS", "Bootstrap", "Javascript", "PHP", "mySQL"],
+  },
+  {
+    year: "Aug 2024",
+    role: "PHP Developer - Final year project",
+    description: `Designed and developed user interfaces for web applications using Bootstrap. Designed and implemented RESTful APIs for data communication. Worked closely with backend to integrate frontend components with PHP mySql. Implemented responsive designs and optimized frontend and backendend perfomance .`,
+    technologies: ["HTML", "CSS", "Bootstrap", "Javascript", "PHP", "mySQL"],
+  },
+  {
+    year: "June 2023 - Present",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    company: "Freelance",
+    description: `Developed and maintained web applications using JavaScript.  Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Employee Management System",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "PHP" , "mySQL"],
   },
   {
-    title: "Task Management App",
+    title: "ElectraDefense",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An application to provide services to secure your home with advanced solutions like CCTV, access control, alarms, and smart automation. Stay protected with real-time monitoring and peace of mind 24/7!",
+    technologies: ["HTML", "CSS", "Bootstrap", "Javascript"],
   },
   {
-    title: "Portfolio Website",
+    title: "React Dashboard",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "A platform to mantain weakly call center sales report.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
   },
   {
-    title: "Blogging Platform",
+    title: "Campus Management System",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "Javascript",  "Bootstrap","Json", "Ajax", "PHP", "mySQL"],
   },
 ];
 
 export const CONTACT = {
-  address: "Islampura Lahore",
+  address: "Lahore",
   phoneNo: "+923211682070 ",
   email: "msaadiftikhar67@gmail.com",
 };
