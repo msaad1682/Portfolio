0fe6810179c0b55ac702698124015a98b6fbc406
msaad1682 <msaadiftikhar67@gmail.com>
2025-02-17 20:12:05 +0500

    profile update

--- src/assets/Components/Hero.jsx
+++ src/assets/Components/Hero.jsx
@@ -3,7 +3,7 @@ import { HERO_CONTENT } from '../../constants'; // Ensure HERO_CONTENT is define
 // import profile from '../../assets/KevinRushProfile.jpg';
 import { motion } from "framer-motion"; // Correct import
 
-import profile from "../../assets/kevinRushProfile.jpg";
+import profile from "../../assets/saad.jpeg";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
